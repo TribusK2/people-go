8410c14f9fe4969d5625798f9012621a344bdad7
Krzysztof Rytlewski <biuro@tribusk2.pl>
2025-01-10 20:30:28 +0100

    file structure update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { NotFoundComponent } from './not-found/not-found.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'offboarding', pathMatch: 'full' },
-  { path: 'offboarding', loadChildren: () => import('./offboarding/offboarding.module').then(m => m.OffboardingModule) },
+  { path: 'offboarding', loadChildren: () => import('./modules/offboarding/offboarding.module').then(m => m.OffboardingModule) },
   { path: '**', component: NotFoundComponent },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { NotFoundComponent } from './not-found/not-found.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
