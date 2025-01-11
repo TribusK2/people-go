535d246f41c5e2160aec82114577c9248b2990f1
Krzysztof Rytlewski <biuro@tribusk2.pl>
2025-01-12 00:16:08 +0100

    update list on offboard

--- src/app/modules/offboarding/employee-details/employee-details.component.ts
+++ src/app/modules/offboarding/employee-details/employee-details.component.ts
@@ -1,8 +1,10 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { Navigation, Router } from '@angular/router';
 
 import { IEmployee } from '../../../interfaces/employee.interface';
 import { ApiService } from '../../../services/api/api.service';
+import { EmployeeTableService } from '../../../services/employee-table/employee-table.service';
+import { EmployeeStatus } from '../../../enums/employee.status.enum';
 
 @Component({
   selector: 'app-employee-details',
@@ -13,9 +15,11 @@ import { ApiService } from '../../../services/api/api.service';
 })
 export class EmployeeDetailsComponent {
   private readonly _apiService: ApiService = inject(ApiService);
+  private readonly _employeeTableService: EmployeeTableService = inject(EmployeeTableService);
   private readonly _router: Router = inject(Router);
 
   public employee = signal<IEmployee | null>(null);
+  public canOffboard = computed<boolean>(() => this._checkIfCanOffboard());
 
   constructor() {
     this._setEmployeeDetails(this._router.getCurrentNavigation());
@@ -26,7 +30,11 @@ export class EmployeeDetailsComponent {
   }
 
   public offboardEmployee(): void {
-    console.log(this.employee());
+    const employeeList = this._employeeTableService.employeeList();
+
+    if (employeeList) {
+      this._updateEmployeeList(employeeList);
+    }
   }
 
   private _setEmployeeDetails(currentNavigation: Navigation | null): void {
@@ -37,6 +45,25 @@ export class EmployeeDetailsComponent {
       this._apiService.getEmployeeDetails(employeeId).subscribe(employee => {
         this.employee.set(employee);
       });
+    }
+  }
+
+  private _checkIfCanOffboard(): boolean {
+    const employee = this.employee();
+    const employeeList = this._employeeTableService.employeeList();
+
+    return !!employeeList && !!employee && employee.status !== EmployeeStatus.OFFBOARDED;
+  }
+
+  private _updateEmployeeList(employeeList: IEmployee[]): void {
+    const employeeIndex = employeeList.findIndex((employee) => {
+      return employee.id === this.employee()?.id;
+    });
+
+    if (employeeIndex > -1) {
+      employeeList[employeeIndex].status = EmployeeStatus.OFFBOARDED;
+      this._employeeTableService.employeeList.set([...employeeList]);
+      this.goToEmployeeList();
     }
   }
 }
