54d476dbf023e1f68d4d46dcd6e9b409bf799dd2
Krzysztof Rytlewski <biuro@tribusk2.pl>
2025-01-12 17:39:30 +0100

    handle error response

--- src/app/interfaces/api-response.interface.ts
+++ src/app/interfaces/api-response.interface.ts
@@ -1,4 +1,5 @@
 export interface IApiResponse<T> {
   data: T;
   code: number;
+  error?: string;
 }
--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { IEmployee } from '../../interfaces/employee.interface';
 import { IApiResponse } from '../../interfaces/api-response.interface';
@@ -12,22 +13,37 @@ import { IUser } from '../../interfaces/user.interface';
 })
 export class ApiService {
   private readonly _httpClient = inject(HttpClient);
+  private readonly _snackBar = inject(MatSnackBar);
 
   public getEmployeeList(): Observable<IEmployee[]> {
     return this._httpClient.get<IApiResponse<IEmployee[]>>('/api/employees').pipe(
-      map(res => res.data)
+      map(res => this._handleResponse(res))
     );
   }
 
   public getEmployeeDetails(employeeId: string): Observable<IEmployee> {
     return this._httpClient.get<IApiResponse<IEmployee>>(`/api/employees/${employeeId}`).pipe(
-      map(res => res.data)
+      map(res => this._handleResponse(res))
     );
   }
 
   public offboardUser(userId: string, offboardData: IOffboardData): Observable<IUser> {
     return this._httpClient.put<IApiResponse<IUser>>(`/api/users/${userId}/offboard`, offboardData).pipe(
-      map(res => res.data)
+      map(res => this._handleResponse(res))
     );
+  }
+
+  private _handleResponse<T>(res: IApiResponse<T>): T {
+    if (res.code !== 200) {
+      this._snackBar.open('Some error occured!', 'Close', {
+        duration: 5000,
+        horizontalPosition: 'center',
+        verticalPosition: 'top',
+      });
+
+      throw new Error(res.error);
+    }
+
+    return res.data;
   }
 }
